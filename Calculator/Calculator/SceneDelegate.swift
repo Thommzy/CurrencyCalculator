27916c93740cf6827aa2c243350ff1d791e1cd4d
timsnap <timothy@snappay.com>
2023-07-13 14:54:42 +0100

    remove default Main. SToryboard and Craeted a Home XIb

---
+++ Calculator/Calculator/SceneDelegate.swift
@@ -0,0 +1,37 @@
+//
+//  SceneDelegate.swift
+//  Calculator
+//
+//  Created by Timothy Obeisun on 7/13/23.
+//
+
+import UIKit
+
+class SceneDelegate: UIResponder, UIWindowSceneDelegate {
+
+    var window: UIWindow?
+
+
+    func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
+        guard let windowScene = (scene as? UIWindowScene) else { return }
+        window = UIWindow(frame: windowScene.coordinateSpace.bounds)
+        window?.windowScene = windowScene
+        let baseVc = HomeViewController(nibName: "HomeViewController", bundle: nil)
+        let navController = UINavigationController(rootViewController: baseVc)
+        window?.rootViewController = navController
+        window?.makeKeyAndVisible()
+    }
+
+    func sceneDidDisconnect(_ scene: UIScene) {}
+
+    func sceneDidBecomeActive(_ scene: UIScene) {}
+
+    func sceneWillResignActive(_ scene: UIScene) {}
+
+    func sceneWillEnterForeground(_ scene: UIScene) {}
+
+    func sceneDidEnterBackground(_ scene: UIScene) {}
+
+
+}
+
---
+++ Calculator/Calculator/Scenes/Home/HomeViewController.swift
@@ -0,0 +1,16 @@
+//
+//  HomeViewController.swift
+//  Calculator
+//
+//  Created by Timothy Obeisun on 7/13/23.
+//
+
+import UIKit
+
+class HomeViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        navigationController?.navigationBar.isHidden = true
+    }
+}
